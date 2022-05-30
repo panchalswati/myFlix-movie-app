7cdbbbd52694986e2941e862d9dbf88b9e3cbef7
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-05-30 14:59:40 +0200

    querying Mongoose for user Model

--- routes/Users.js
+++ routes/Users.js
@@ -1,83 +1,126 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-let users = [
-    {
-        "id": "1",
-        "name": "swati",
-        "favouriteMovies": []
-    },
-    {
-        "id": "2",
-        "name": "John",
-        "favouriteMovies": [
-            "The Dark Knight"
-        ]
-    }
-]
+const User = require('../models/user');
+
+//read all users
 router.get('/', (req, res) => {
-    res.status(200).json(users);
-});
-
-router    //create new user
-    .post('/', (req, res) => {
-        const newUser = req.body;
-        if (newUser.name) {
-            newUser.id = uuid.v4();
-            users.push(newUser);
-            res.status(201).json(newUser)
-        } else {
-            res.status(400).send("users need name");
-        }
-    });
-
-//update user
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find(user => user.id == id);
-    if (user) {
-        user.name = updatedUser;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send("No such user")
-    }
-});
-
-router.post('/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-
-    let user = users.find(user => user.id == id);
-    if (user) {
-        user.favouriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-    } else {
-        res.status(400).send("No such user");
-    }
+    User.find()
+        .then((user) => {
+            res.status(200).json(user);
+        })
+        .catch((error) => {
+            res.status(500).send('Error' + error);
+        })
 })
 
-router.delete('/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
+//insert new user
+router.post('/', (req, res) => {
+    User.findOne({ name: req.body.name })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.name + ' already exists');
+            } else {
+                User
+                    .create({
+                        name: req.body.name,
+                        password: req.body.password,
+                        email: req.body.email,
+                        Birthdate: req.body.Birthdate,
+                    })
+                    .then(user => { res.status(201).json(user) })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error' + error);
+                    })
+            }
+        }).catch((error) => {
+            console.error(error);
+            res.status(500).send('Error' + error);
+        })
+})
 
-    let user = users.find(user => user.id == id);
-    if (user) {
-        user.favouriteMovies = user.favouriteMovies.filter(title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-    } else {
-        res.status(400).send("No such user");
-    }
+//read user by name
+router.get('/:name', (req, res) => {
+    User.findOne({ name: req.params.name })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    let user = users.find(user => user.id == id);
-    if (user) {
-        users = users.filter(user => user.id !== id);
-        res.status(200).send(`user email with ${id} is removed`);
-    } else {
-        res.status(400).send("No such user");
-    }
+//update user by name
+router.put('/:name', (req, res) => {
+    User.findOneAndUpdate({ name: req.params.name }, {
+        $set:
+        {
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            Birthdate: req.body.Birthdate,
+            favoriteMovies: req.body.favoriteMovies
+        }
+    },
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
 });
+
+//insert movie to user's favorite Movies list
+router.post('/:name/movies/:MovieID', (req, res) => {
+    User.findOneAndUpdate({ name: req.params.name }, {
+        $push: { favoriteMovies: req.params.MovieID }
+    },
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+
+router.delete('/:name/movies/:MovieID', (req, res) => {
+    User.findOneAndUpdate({ name: req.params.name }, {
+        $pull: { favoriteMovies: req.params.MovieID }
+    },
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+
+//delete - deregister user by name
+router.delete('/:name', (req, res) => {
+    User.findOneAndRemove({ name: req.params.name })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.name + ' was not found');
+            } else {
+                res.status(200).send(req.params.name + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
 module.exports = router;
