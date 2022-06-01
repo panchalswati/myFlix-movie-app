0da37fbde3392dd9d7a652a0aa05f65bb21c73b9
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-01 12:36:34 +0200

    added validation logic for requests

--- users/Users.route.js
+++ users/Users.route.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const User = require('./user.model');
 
 //read all users
-router.get('/', (req, res) => {
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     User.find()
         .then((user) => {
             res.status(200).json(user);
@@ -16,33 +16,45 @@ router.get('/', (req, res) => {
 })
 
 //insert new user
-router.post('/', (req, res) => {
-    User.findOne({ name: req.body.name })
-        .then((user) => {
-            if (user) {
-                return res.status(400).send(req.body.name + ' already exists');
-            } else {
-                User
-                    .create({
-                        name: req.body.name,
-                        password: req.body.password,
-                        email: req.body.email,
-                        Birthdate: req.body.Birthdate,
-                    })
-                    .then(user => { res.status(201).json(user) })
-                    .catch((error) => {
-                        console.error(error);
-                        res.status(500).send('Error' + error);
-                    })
-            }
-        }).catch((error) => {
-            console.error(error);
-            res.status(500).send('Error' + error);
-        })
-})
+router.post('/', [
+    check('name', 'Username is required').isLength({ min: 5 }),
+    check('name', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('password', 'Password is required').not().isEmpty(),
+    check('email', 'Email does not appear to be valid').isEmail()
+],
+    (req, res) => {
+        let errors = validationResult(req);  //check validation object for error
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        let hashedPassword = User.hashPassword(req.body.password);    //hashed password
+        User.findOne({ name: req.body.name })
+            .then((user) => {
+                if (user) {
+                    return res.status(400).send(req.body.name + ' already exists');
+                } else {
+                    User
+                        .create({
+                            name: req.body.name,
+                            password: hashedPassword,
+                            email: req.body.email,
+                            Birthdate: req.body.Birthdate,
+                        })
+                        .then(user => { res.status(201).json(user) })
+                        .catch((error) => {
+                            console.error(error);
+                            res.status(500).send('Error' + error);
+                        })
+                }
+            }).catch((error) => {
+                console.error(error);
+                res.status(500).send('Error' + error);
+            })
+    })
 
 //read user by name
-router.get('/:name', (req, res) => {
+router.get('/:name', passport.authenticate('jwt', { session: false }), (req, res) => {
     User.findOne({ name: req.params.name })
         .then((user) => {
             res.json(user);
@@ -54,30 +66,42 @@ router.get('/:name', (req, res) => {
 });
 
 //update user by name
-router.put('/:name', (req, res) => {
-    User.findOneAndUpdate({ name: req.params.name }, {
-        $set:
-        {
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            Birthdate: req.body.Birthdate,
-            favoriteMovies: req.body.favoriteMovies
+router.put('/:name', [
+    check('name', 'Username is required').isLength({ min: 5 }),
+    check('name', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('password', 'Password is required').not().isEmpty(),
+    check('email', 'Email does not appear to be valid').isEmail()
+],
+    (req, res) => {
+        let errors = validationResult(req);  //check validation object for error
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
         }
-    },
-        { new: true },
-        (err, updatedUser) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error: ' + err);
-            } else {
-                res.json(updatedUser);
+        let hashedPassword = User.hashPassword(req.body.password);
+        User.findOneAndUpdate({ name: req.params.name }, {
+            $set:
+            {
+                name: req.body.name,
+                password: hashedPassword,
+                email: req.body.email,
+                Birthdate: req.body.Birthdate,
+                favoriteMovies: req.body.favoriteMovies
             }
-        });
-});
+        },
+            { new: true },
+            (err, updatedUser) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error: ' + err);
+                } else {
+                    res.json(updatedUser);
+                }
+            });
+    });
 
 //insert movie to user's favorite Movies list
-router.post('/:name/movies/:MovieID', (req, res) => {
+router.post('/:name/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     User.findOneAndUpdate({ name: req.params.name }, {
         $push: { favoriteMovies: req.params.MovieID }
     },
@@ -92,7 +116,7 @@ router.post('/:name/movies/:MovieID', (req, res) => {
         });
 });
 
-router.delete('/:name/movies/:MovieID', (req, res) => {
+router.delete('/:name/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     User.findOneAndUpdate({ name: req.params.name }, {
         $pull: { favoriteMovies: req.params.MovieID }
     },
@@ -108,7 +132,7 @@ router.delete('/:name/movies/:MovieID', (req, res) => {
 });
 
 //delete - deregister user by name
-router.delete('/:name', (req, res) => {
+router.delete('/:name', passport.authenticate('jwt', { session: false }), (req, res) => {
     User.findOneAndRemove({ name: req.params.name })
         .then((user) => {
             if (!user) {
