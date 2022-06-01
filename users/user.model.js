2502446ca039174be54c265949ca940dc2a08b15
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-01 12:34:39 +0200

    added hashPassword() and validatePassword()

--- users/user.model.js
+++ users/user.model.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = mongoose.Schema({
-    name: String,
-    password: String,
-    email: String,
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
     Birthdate: Date,
     favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
