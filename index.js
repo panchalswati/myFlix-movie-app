bac6fc8bae8ddb781136241627fea6a9257019cf
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-02 14:21:47 +0200

    added express-validator and cors

--- index.js
+++ index.js
@@ -3,29 +3,22 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     mongoose = require('mongoose'),
-    cors = require('cors');
-app.use(cors());
-
-const port = process.env.PORT || 8080;
-
-MoviesRoutes = require('./movies/Movies.route.js');
-UsersRoutes = require('./users/Users.route.js');
+    MoviesRoutes = require('./movies/Movies.route.js'),
+    UsersRoutes = require('./users/Users.route.js');
 app.use(express.json());
 app.use(morgan('dev'));
-
+const { check, validationResult } = require('express-validator');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const cors = require('cors');
+app.use(cors());
 
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
-
-const { check, validationResult } = require('express-validator');
-
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
 app.use('/movies', MoviesRoutes);
-
 app.use('/users', UsersRoutes);
 
 app.get('/', (req, res, next) => {
@@ -34,13 +27,12 @@ app.get('/', (req, res, next) => {
 });
 //serving static files
 app.use(express.static('public'));
-
 //error-handling middleware library
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
-
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
     console.log('Listening on Port ' + port);
 });
