d058cf8a0f4f4e3995554d3f11be2f416cff3282
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-02 15:16:45 +0200

    connected database to API on Heroku

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(cors());
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use('/movies', MoviesRoutes);
 app.use('/users', UsersRoutes);
 
