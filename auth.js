e650d4bd7a06bb97ac8334f97884d3d62c3b2ef6
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-07 14:22:34 +0200

    changed name  of passport.js to localpassport.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the 
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
 
-require('./passport'); // Your local passport file
+require('./localPassport'); // Your local passport file
 
 
 let generateJWTToken = (user) => {
