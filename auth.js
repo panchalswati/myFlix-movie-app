c6f3990482418d803bafef9df371c7d0187a683a
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-01 14:16:23 +0200

    changed database field name

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ require('./localPassport'); // Your local passport file
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject: user.name, // This is the username you’re encoding in the JWT
+        subject: user.Username, // This is the username you’re encoding in the JWT
         expiresIn: '7d', // This specifies that the token will expire in 7 days
         algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
     });
