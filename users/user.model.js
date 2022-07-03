7bba6cf407995455f390d57d0c8a40ef20a2bce8
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-03 09:28:34 +0200

    added date dtatype for Birthdate field

--- users/user.model.js
+++ users/user.model.js
@@ -5,7 +5,7 @@ let userSchema = mongoose.Schema({
     Username: { type: String, required: true },
     Password: { type: String, required: true },
     Email: { type: String, required: true },
-    Birthdate: { type: Number, required: true },
+    Birthdate: { type: Date, required: true },
     FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
